5ee7f9f4d8309beac9af35af0884d1c11c29db90
cancure <dantis@msn.com>
2016-07-03 00:34:11 +0530

    Added comments to OAuth2ServerConfiguration.java about how to use oauth2 token to get access to REST API.

--- src/main/java/org/cancure/cpa/security/oauth2/OAuth2ServerConfiguration.java
+++ src/main/java/org/cancure/cpa/security/oauth2/OAuth2ServerConfiguration.java
@@ -20,6 +20,37 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
+/**
+ *
+ * After you launch the app, you can seek a bearer token like this:
+ *
+ * <pre>
+ * POST to URL - http://localhost:8080/oauth/token?password=cancure&username=cancure&grant_type=password&scope=read%20write&client_secret=cancure123456&client_id=cancureapp
+ * Using Basic Auth - Username = cancureapp and Password = cancure123456
+ * i.e. Base64 Encode the username:password and set the header "Authorization" with value "Basic <base64 encoded username:password>".
+ * E.g. Base64 encoded value of cancureapp:cancure123456 = Y2FuY3VyZWFwcDpjYW5jdXJlMTIzNDU2
+ * Set Header as Authorization=Basic Y2FuY3VyZWFwcDpjYW5jdXJlMTIzNDU2
+ * 
+ * Response should be similar to this:
+ * <code>{"access_token":"533de99b-5a0f-4175-8afd-1a64feb952d5","token_type":"bearer","expires_in":43199,"scope":"read"}</code>
+ *
+ * </pre>
+ * 
+ * With the token value, you can now interrogate the RESTful interface like this:
+ *
+ * <pre>
+ * curl -H "Authorization: bearer <access_token>" localhost:8080/url
+ * </pre>
+ * 
+ * After the specified time period, the access_token will expire. Use the refresh_token that was returned in the original OAuth authorization to retrieve a new access_token:
+ * 
+ * <code>
+ * curl -X POST -vu clientapp:cancure123456 http://localhost:8080/oauth/token?grant_type=refresh_token&refresh_token=f554d386-0b0a-461b-bdb2-292831cecd57&client_secret=cancure123456&client_id=cancureapp -H "Accept: application/json"
+ * </code>
+ * 
+ * @author 870154
+ *
+ */
 @Configuration
 public class OAuth2ServerConfiguration {
 
