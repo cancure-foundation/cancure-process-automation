e330e40a72cf796f4f5c7a0a9c617978c7cb883b
194729 <194729@01HW750167.India.TCS.com>
2016-07-08 18:59:18 +0530

    UserController

---
+++ src/main/java/org/cancure/cpa/controller/UserController.java
@@ -0,0 +1,27 @@
+package org.cancure.cpa.controller;
+
+import org.cancure.cpa.persistence.entity.User;
+import org.cancure.cpa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping("/user/save")
+	public User addUser(@RequestBody User user) {
+		return userService.addUser (user);
+
+	}
+
+	@RequestMapping("/user/list")
+	public Iterable<User> listUsers() {
+		return userService.listUsers ();
+	}
+
+}
