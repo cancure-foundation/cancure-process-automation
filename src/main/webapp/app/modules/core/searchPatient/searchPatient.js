a0bc273cdbbeff53355b75a56a6b6631af0790f3
nidhingeorgejoseph <nidhin.joseph1@gmail.com>
2016-08-12 18:21:10 +0530

    UI and logic fix in search patient page

--- src/main/webapp/app/modules/core/searchPatient/searchPatient.js
+++ src/main/webapp/app/modules/core/searchPatient/searchPatient.js
@@ -1,46 +1,45 @@
 core.controller("SearchPatientController", ['$rootScope', '$scope', '$state', '$stateParams', 'Flash', 'apiService', 'appSettings',
-function ($rootScope, $scope, $state, $stateParams, Flash, apiService, appSettings) {
-    
-	var vm = this;
-	 vm.formData = {};
+                                            function ($rootScope, $scope, $state, $stateParams, Flash, apiService, appSettings) {
 
-     var init = function () {
-       vm.formData = {};
-     };
+	var vm = this;	
+	var init = function () {
+		vm.formData = {};
+	};
 
-     // init function, execution starts here
-     init();
+	// init function, execution starts here
+	init();
 
-     //function to handle save button click
-     vm.submitForm = function () {
-    	 
-    	 var url = '';
-    	 Flash.create('info', 'Please wait while we Search patient.', 'large-text');    
-         
-    	 if (vm.formData.searchBy == 'prn') {
-        	 if (isNaN(vm.formData.searchText)) {
-        		 Flash.create('danger', 'PRN should be numeric. Please enter again.', 'large-text');
-        		 return;
-        	 } else {
-        		 url = 'patient/' +  vm.formData.searchText;
-        	 }
-        	 
-         } else if (vm.formData.searchBy == 'name') {
-        	 url = 'patient/search/' +  vm.formData.searchText;
-         }
-         
-         apiService.serviceRequest({
-        	 URL: url,
-        	 method:'GET',
-        }, function (response) {
-        	vm.patientList = response;
-        });
-     }
-     
-     
-     vm.changeView = function(prn){
-	    $state.go('app.patientRegHistory', { prn: prn });
-	 }
-     
-     
+	//function to handle save button click
+	vm.submitForm = function () {
+
+		var url = 'patient/search/' +  vm.formData.searchText;
+		delete vm.patientList; //deletes the patient list so as to hide the search result container
+
+		Flash.create('info', 'Please wait while we Search patient.', 'large-text');    
+
+		if (vm.formData.searchBy == 'prn') {
+			if (isNaN(vm.formData.searchText)) {
+				Flash.create('danger', 'PRN should be numeric. Please enter again.', 'large-text');
+				return;
+			} else 
+				url = 'patient/' +  vm.formData.searchText;        	         	 
+		} 
+
+		apiService.serviceRequest({
+			URL: url
+		}, function (response) {
+			Flash.create('success', 'Search completed with ' + response.length + ' results', 'large-text');
+			vm.patientList = response;
+			setTimeout(function (){
+				var centerContent = document.getElementById('center-content-wrapper');
+				if (centerContent)
+					centerContent.scrollTop += 500; // scrolls view to search result area
+			}, 100);
+		});
+	};     
+	//
+	vm.changeView = function(prn){
+		$state.go('app.patientRegHistory', { prn: prn });
+	}
+
 }]);
