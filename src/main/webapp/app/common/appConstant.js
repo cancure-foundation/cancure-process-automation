833e71ba1c3b20067bd4e29e1cabc5a20f349e7d
cancure <dantis@msn.com>
2016-08-22 10:26:11 +0530

    Hospital UI and backend created.

--- src/main/webapp/app/common/appConstant.js
+++ src/main/webapp/app/common/appConstant.js
@@ -91,6 +91,11 @@ app.constant('appSettings', {
             title: "Patient Search",
             icon: "search",
             state: "searchPatient"
+        },
+	    {
+            title: "Hospitals",
+            icon: "search",
+            state: "hospital.hospitalList"
         }
 	]
 });
