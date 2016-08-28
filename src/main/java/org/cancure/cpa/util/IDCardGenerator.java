a595e4e3363f3b1ac1472bddb890587d9047f12c
cancure <dantis@msn.com>
2016-08-28 11:05:49 +0530

    Enhancements to IDCard generator

--- src/main/java/org/cancure/cpa/util/IDCardGenerator.java
+++ src/main/java/org/cancure/cpa/util/IDCardGenerator.java
@@ -1,27 +1,25 @@
 package org.cancure.cpa.util;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.cancure.cpa.controller.beans.PatientBean;
 import org.cancure.cpa.controller.beans.PatientDocumentBean;
-import org.cancure.cpa.persistence.entity.PatientDocument;
 import org.cancure.cpa.service.PatientDocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfGState;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class IDCardGenerator {
@@ -51,7 +49,7 @@ public class IDCardGenerator {
 
 	private void generateCard(PatientBean patient) throws Exception {
 	
-		Rectangle pagesize = new Rectangle(400f, 272f);
+		Rectangle pagesize = new Rectangle(400f, 245f);
 		Document document = new Document(pagesize, 10f, 72f, 108f, 180f);
 
 		String imageLoc = "src/main/webapp/images/logo.jpg";
@@ -62,19 +60,22 @@ public class IDCardGenerator {
 				new FileOutputStream("C:\\Users\\sudo\\cancure\\cancuepdf.pdf"));
 		// step 3
 		document.open();
-
 		PdfContentByte canvas = writer.getDirectContent();
 		
-		printText(canvas, 33, 272-38, "CANCURE Foundation", 20, 1.5f);
-		printText(canvas, 48, 272-76, "Name", 10, 0.2f);
-		printText(canvas, 48, 272-92, patient.getName(), 15, 1f);
+		printText(canvas, 137, 245-32, "CANCURE Foundation", 20, 1.5f);
+		printText(canvas, 137, 245-48, "Regd. Office: 60/3285, Benrub, P. K. Devoor Road, ", 9, 0.2f);
+		printText(canvas, 204, 245-63, "Perumanoor, Cochin -15 ", 9, 0.2f);
+			
+		printText(canvas, 141, 245-138, "Name : ", 10, 0.2f);
+		printText(canvas, 199, 245-138, patient.getName(), 12, 1f);
 		
-		printText(canvas, 48, 272-107, "Date of Birth", 10, 0.2f);
-		printText(canvas, 48, 272-123, patient.getDob().toString(), 15, 1f);
+		printText(canvas, 154, 245-157, "DOB : ", 10, 0.2f);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy");
+		printText(canvas, 199, 245-157, sdf.format(patient.getDob()), 12, 1f);
 		
-		printText(canvas, 48, 272-143, "Gender", 10, 0.2f);
+		/*printText(canvas, 48, 272-143, "Gender", 10, 0.2f);
 		printText(canvas, 48, 272-159, patient.getGender(), 15, 1f);
-		
+		*/
 		/*PatientDocument patDoc = patientDocumentService.findOne(id);
         String filePath = fileSavePath + patDoc.getDocPath();
         InputStream is = new FileInputStream(filePath);
@@ -91,10 +92,11 @@ public class IDCardGenerator {
 		
 		if (docPath != null) {
 			String filePath = basePath + "/" +  docPath;
-			printImage(canvas, 285, 20, 100, 100, filePath);
+			printImage(canvas, 10, 245 - 232, 114, 126, filePath);
 		}
 		
-		printImage(canvas, 285, 180, 100, 100, imageLoc);
+		printImage(canvas, 10, 245-96, 100, 100, imageLoc);
+		printTransparentImage(canvas, 127, 245-232, 273, 226, imageLoc);
 		
 		// step 5
 		document.close();
@@ -102,10 +104,25 @@ public class IDCardGenerator {
 	
 	private void printImage(PdfContentByte canvas, float x, float y, float scalex, float scaley, String imageLoc)
 			throws MalformedURLException, IOException, DocumentException {
+		canvas.saveState();
 		Image cancureLog = Image.getInstance(imageLoc);
 		cancureLog.setAbsolutePosition(x, y);
 		cancureLog.scaleToFit(scalex, scaley);
 		canvas.addImage(cancureLog);
+		canvas.restoreState();
+	}
+	
+	private void printTransparentImage(PdfContentByte canvas, float x, float y, float scalex, float scaley, String imageLoc)
+			throws MalformedURLException, IOException, DocumentException {
+		Image cancureLog = Image.getInstance(imageLoc);
+		canvas.saveState();
+		PdfGState state = new PdfGState();
+		state.setFillOpacity(0.05f);
+		canvas.setGState(state);
+		cancureLog.setAbsolutePosition(x, y);
+		cancureLog.scaleToFit(scalex, scaley);
+		canvas.addImage(cancureLog);
+		canvas.restoreState();
 	}
 	
 	private void printText(PdfContentByte canvas, float x, float y, String text, float fontSize, float lineWidth) throws DocumentException, IOException {
