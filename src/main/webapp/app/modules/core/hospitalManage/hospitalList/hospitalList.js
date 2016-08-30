d9701909d335b1411ccd6511051d97023b40500b
nidhingeorgejoseph <nidhin.joseph1@gmail.com>
2016-08-30 12:33:09 +0530

    - UI fixes in doc and hosp page - added loader in these two pages

--- src/main/webapp/app/modules/core/hospitalManage/hospitalList/hospitalList.js
+++ src/main/webapp/app/modules/core/hospitalManage/hospitalList/hospitalList.js
@@ -1,21 +1,21 @@
 core.controller("HospitalListController", ['$scope', '$state', 'Loader', 'apiService', 'appSettings', 
-function ($scope, $state, Loader, apiService, appSettings) {
-        var vm = this;
-        
-        var init = function () {
-        	Loader.create('Fetching Data. Please wait');
-        	apiService.serviceRequest({
-                URL: appSettings.requestURL.hospitalList,
-                errorMsg: 'Failed to fetch hospitals. Try Again!'
-            }, function (response) {
-            	Loader.destroy();
-            	$scope.hospitalList = response;
-            })
-        };
-        
-        vm.editHospital = function(id){
-        	$state.go('app.hospital.hospitalCreate', { hospitalId: id });
-        }     
-            
-        init();
+                                           function ($scope, $state, Loader, apiService, appSettings) {
+	var vm = this;
+
+	var init = function () {
+		Loader.create('Fetching Data. Please wait');
+		apiService.serviceRequest({
+			URL: appSettings.requestURL.hospitalList,
+			errorMsg: 'Failed to fetch hospitals. Try Again!'
+		}, function (response) {
+			Loader.destroy();
+			$scope.hospitalList = response;
+		})
+	};
+
+	vm.editHospital = function(id){
+		$state.go('app.hospital.hospitalCreate', { hospitalId: id });
+	}     
+
+	init();
 }]);
