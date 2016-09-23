e67e41c97518750f43c2c3fcdcc6d44aba0fbe90
nidhingeorgejoseph <nidhin.joseph1@gmail.com>
2016-09-23 11:28:33 +0530

    QA Fixes

--- src/main/webapp/app/modules/core/hospitalManage/hpocMapping/hpocMapping.js
+++ src/main/webapp/app/modules/core/hospitalManage/hpocMapping/hpocMapping.js
@@ -4,7 +4,7 @@ core.controller("HpocMappingController", ['$q', '$timeout', '$scope', '$state', 
 
 	var init = function () {
 		Loader.create('Fetching Data. Please wait');
-		
+
 		var hospitalList = apiService.asyncServiceRequest({URL : appSettings.requestURL.hospitalList}); // for hosptial list
 		var hpocList = apiService.asyncServiceRequest({URL : appSettings.requestURL.hpocList}); // for HPOC list
 		var hpocMappedList = apiService.asyncServiceRequest({URL : appSettings.requestURL.hospitalHpoc}); // for getting already mapped data
@@ -13,7 +13,7 @@ core.controller("HpocMappingController", ['$q', '$timeout', '$scope', '$state', 
 			vm.hospitalList = success[0];
 			vm.hpocList = success[1];
 			vm.hospitalHpocList = success[2];
-			
+
 			$timeout(function (){
 				$('#hpocMapping-panel-expandable').slideToggle(); // toggles the expandable panel
 				Loader.destroy();				
@@ -28,7 +28,7 @@ core.controller("HpocMappingController", ['$q', '$timeout', '$scope', '$state', 
 			Flash.create('warning', 'Please select all options to procees.', 'large-text');   
 			return
 		}
-		Loader.create('Please wait while we register you...');
+		Loader.create('Please wait while HPOC is mapped...');
 
 		// making the server call
 		apiService.serviceRequest({
@@ -37,7 +37,8 @@ core.controller("HpocMappingController", ['$q', '$timeout', '$scope', '$state', 
 			payLoad: {
 				hospitalId : vm.hospitalListValue,
 				hpocIdList : vm.hpocListValue
-			}
+			},
+			errorMsg : 'Error Mapping selected HPOC to hospital. Please try again!'
 		}, function (response) {
 			Loader.destroy();
 			delete vm.hospitalListValue;
@@ -50,10 +51,27 @@ core.controller("HpocMappingController", ['$q', '$timeout', '$scope', '$state', 
 	/**
 	 * toggles the expandable panel
 	 */
-	vm.togglePanel = function (){
-		vm.panelExpanded = !vm.panelExpanded;
-		$('#hpocMapping-panel-expandable').slideToggle();
+	vm.togglePanel = function (action){
+		vm.panelExpanded = !vm.panelExpanded; // toggles the flag to indicate panel state
+		$('#hpocMapping-panel-expandable').slideToggle(); // toggles the panel
+		if (action == "close"){
+			vm.editMode = false; // sets off the edit flag
+			vm.hospitalListValue = null; // clears the hospital list
+			vm.hpocListValue = []; // clears the hpoc list
+		}
 	};
-	
+	/**
+	 *  function to handle edit functionality
+	 */
+	vm.editHpoc = function (values){			
+		vm.editMode = true; // sets the edit mode flag
+		vm.hospitalListValue = values.hospital.hospitalId; // sets values for hospital drop-down
+		vm.hpocListValue = [];
+		for (var i=0; i < values.hpocList.length; i++)
+			vm.hpocListValue.push(values.hpocList[i].id); // sets values for hpoc drop-down
+		if(!vm.panelExpanded) // checks if panel is expanded or not and expands if not
+			vm.togglePanel();
+	}
+
 	init();
 }]);
