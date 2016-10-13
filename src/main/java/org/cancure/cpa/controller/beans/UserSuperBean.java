1f9e664d597d5839d69c250c843c49a7ec83eadb
979702 <979702@01HW811082.India.TCS.com>
2016-10-13 15:52:31 +0530

    Checked out from ui-nidhin.

--- src/main/java/org/cancure/cpa/controller/beans/UserSuperBean.java
+++ src/main/java/org/cancure/cpa/controller/beans/UserSuperBean.java
@@ -8,71 +8,71 @@ import org.cancure.cpa.persistence.entity.Role;
 
 public class UserSuperBean {
 
-    private Integer id;
+	private Integer id;
 
-    private String name;
+	private String name;
 
-    private String login;
+	private String login;
 
-    private Set<Role> roles = new HashSet<Role>();
-    
-    private Boolean enabled;
-    
-    private String email;
-    
-    private Boolean firstLog;
-    
-    private Doctor doctor;
-    
-    private Integer hospitalId;
+	private Set<Role> roles = new HashSet<Role>();
+	
+	private Boolean enabled;
+	
+	private String email;
+	
+	private Boolean firstLog;
+	
+	private Doctor doctor;
+	
+	private Integer hospitalId;
 
-    public Set<Role> getRoles() {
-        return roles;
-    }
+	public Set<Role> getRoles() {
+		return roles;
+	}
 
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getLogin() {
-        return login;
-    }
+	public String getLogin() {
+		return login;
+	}
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+	public void setLogin(String login) {
+		this.login = login;
+	}
 
-    public Boolean getEnabled() {
-        return enabled;
-    }
+	public Boolean getEnabled() {
+		return enabled;
+	}
 
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
     public Boolean getFirstLog() {
         return firstLog;
@@ -82,21 +82,21 @@ public class UserSuperBean {
         this.firstLog = firstLog;
     }
 
-    public Doctor getDoctor() {
-        return doctor;
-    }
+	public Doctor getDoctor() {
+		return doctor;
+	}
 
-    public void setDoctor(Doctor doctor) {
-        this.doctor = doctor;
-    }
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
 
-    public Integer getHospitalId() {
-        return hospitalId;
-    }
+	public Integer getHospitalId() {
+		return hospitalId;
+	}
 
-    public void setHospitalId(Integer hospitalId) {
-        this.hospitalId = hospitalId;
-    }
+	public void setHospitalId(Integer hospitalId) {
+		this.hospitalId = hospitalId;
+	}
 
     
 }
--- src/main/webapp/app/modules/core/coreConfig.js
+++ src/main/webapp/app/modules/core/coreConfig.js
@@ -26,7 +26,7 @@ core.config(["$stateProvider", function ($stateProvider) {
     });
     //  Create User
     $stateProvider.state('app.manageUser.createUser', {
-        url: '/createUser/:userId',
+        url: '/createUser/:user',
         templateUrl: 'app/modules/core/manageUser/createUser/createUser.html',
         controller: 'CreateUserController',
         controllerAs: 'vm',
