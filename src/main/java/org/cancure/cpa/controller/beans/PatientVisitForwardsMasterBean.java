4310f2f5233f9bbb7bd4a89cfb4e06a5207abe3b
cancure <dantis@msn.com>
2016-11-29 07:51:59 +0530

    Re-adding missed files

---
+++ src/main/java/org/cancure/cpa/controller/beans/PatientVisitForwardsMasterBean.java
@@ -0,0 +1,16 @@
+package org.cancure.cpa.controller.beans;
+
+import java.util.List;
+
+public class PatientVisitForwardsMasterBean {
+
+	private List<PatientVisitForwardsBean> patientVisitForwards;
+
+	public List<PatientVisitForwardsBean> getPatientVisitForwards() {
+		return patientVisitForwards;
+	}
+
+	public void setPatientVisitForwards(List<PatientVisitForwardsBean> patientVisitForwards) {
+		this.patientVisitForwards = patientVisitForwards;
+	}
+}
