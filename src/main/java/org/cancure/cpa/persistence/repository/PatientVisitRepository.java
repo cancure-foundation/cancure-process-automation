f4710584fbc32e7086a37ae4f76e934594b77fb0
psda01 <dantis.ps@nielsen.com>
2016-12-14 12:05:55 +0530

    workflowExists boolean will be returned when a patient is searched for patient visit

--- src/main/java/org/cancure/cpa/persistence/repository/PatientVisitRepository.java
+++ src/main/java/org/cancure/cpa/persistence/repository/PatientVisitRepository.java
@@ -1,8 +1,14 @@
 package org.cancure.cpa.persistence.repository;
 
+import java.util.List;
+
 import org.cancure.cpa.persistence.entity.PatientVisit;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PatientVisitRepository extends CrudRepository<PatientVisit, Integer>  {
+	
+	@Query("from PatientVisit p where pidn = ?1 and status = 'open' ")
+	public List<PatientVisit> findOpenWorkflowsOfPatient(Integer pidn);
 
 }
