a4b370f2721a6ab32dc7b3efecec65564c928345
nidhingeorgejoseph <nidhin.joseph1@gmail.com>
2016-12-19 13:00:24 +0530

    UI Fixes

--- src/main/webapp/app/common/appConstant.js
+++ src/main/webapp/app/common/appConstant.js
@@ -144,10 +144,10 @@ app.constant('appSettings', {
 		ROLE_GUEST : [0, 9],
 		ROLE_SECRETARY : [0, 9],
 		ROLE_EXECUTIVE_COMMITTEE : [0, 9],
-		ROLE_ADMIN : [0, 1, 3, 4, 5, 6, 7, 8, 10],
+		ROLE_ADMIN : [0, 1, 3, 4, 5, 6, 7, 10],
 		ROLE_HOSPITAL_POC : [0, 2, 7, 9], 
 		ROLE_DOCTOR : [0, 9],
-		ROLE_PROGRAM_COORDINATOR : [0, 2, 8, 9],
+		ROLE_PROGRAM_COORDINATOR : [0, 2, 9],
 		ROLE_PHARMACY : [8, 9]
 	},
 	
