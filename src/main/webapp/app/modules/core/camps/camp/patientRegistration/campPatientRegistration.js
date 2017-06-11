d42f79d84a10951608c4d165f6d2f8a1a0a1387a
cancure <dantis@msn.com>
2017-06-11 22:18:04 +0530

    Camp search before registering people.

--- src/main/webapp/app/modules/core/camps/camp/patientRegistration/campPatientRegistration.js
+++ src/main/webapp/app/modules/core/camps/camp/patientRegistration/campPatientRegistration.js
@@ -1,7 +1,12 @@
 core.controller("CampPatientRegisterController", ['$scope', '$timeout', '$stateParams', 'Flash', 'apiService', 'appSettings', 'Loader',
                                          function ($scope, $timeout, $stateParams, Flash, apiService, appSettings, Loader) {
 	var vm = this;
+	vm.campSearched = false;
+	vm.campSearchForm = {};
+	vm.selectedCamp = {};
 	vm.formData = {};
+	vm.camps = [];
+	
 	vm.alllabtests = [{"listValueId":"4","listId":1,"value":"Daughter"},{"listValueId":"8","listId":1,"value":"Mother"},{"listValueId":"7","listId":1,"value":"Father"},{"listValueId":"3","listId":1,"value":"Son"},{"listValueId":"1","listId":1,"value":"Wife"},{"listValueId":"9","listId":1,"value":"Friend"},{"listValueId":"5","listId":1,"value":"Sister"},{"listValueId":"2","listId":1,"value":"Husband"},{"listValueId":"10","listId":1,"value":"Well Wisher"},{"listValueId":"6","listId":1,"value":"Brother"}];
 	vm.allgenders = [{
 		value : 'Male'
@@ -25,6 +30,27 @@ core.controller("CampPatientRegisterController", ['$scope', '$timeout', '$stateP
 		}
 	}
 
+	vm.searchCamp = function() {
+		vm.campSearched = true;
+		vm.camps = [{"campId": 1, "campName" : "Thrikkakara", "campPlace" : "Kakkanad", "localPOC": "Ajith"}];
+	}
+
+	vm.selectCamp = function(id) {
+		for (var i=0; i < vm.camps.length; i++) {
+			if (vm.camps[i].campId == id) {
+				vm.selectedCamp = vm.camps[i];
+				vm.campSelected = true;
+				break;
+			}
+		}
+	}
 	
+	vm.resetCamp = function() {
+		vm.camps = [];
+		vm.campSearched = false;
+		vm.selectedCamp = {};
+		vm.campSelected = false;
+		vm.campSearchForm = {};
+	}
 	
 }]);
